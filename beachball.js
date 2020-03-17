3eff3b7eb9111f50cecd443b05ed535cf3eda32d
mathias <math06j6@stud.kea.dk>
2020-03-17 19:02:55 +0100

    static

--- beachball.js
+++ beachball.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// const data = [
+//   {
+//     i: "beachball.svg"
+//   }
+// ];
+// data.forEach(d => {
+//   const t = document.createElement("svg");
+//   t.src = "svg/" + d.i;
+//   document.body.appendChild(t);
+// });
+
