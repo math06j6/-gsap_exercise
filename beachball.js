76eb8ae37586d242481d2b7ea70ce8925fad496f
mathias <math06j6@stud.kea.dk>
2020-03-17 22:19:49 +0100

    gsap

--- beachball.js
+++ beachball.js
@@ -3,4 +3,9 @@ import { gsap } from "gsap";
 
 let tl = gsap.timeline();
 
-tl.to(".ball", { duration: 2, x: 400, rotate: 200, duration: 2, ease: "slow" }).to(".ball", { duration: 2, y: -22, x: 413, rotate: 360 });
+tl.to(".ball", { duration: 2, x: 400, rotate: 200, duration: 2, ease: "slow" })
+  .to(".ball", { duration: 2, y: 10, x: 390, rotate: 360, scale: 0.85, ease: "slow" })
+  .to(".ball", { duration: 0.35, y: 15, x: 388, scale: 0.75, ease: "slow" })
+  .to(".ball", { duration: 0.3, y: 40, x: 385, scale: 0.55, ease: "slow" })
+  .to(".ball", { duration: 0.2, y: 40, x: 385, scale: 0.25 })
+  .to(".ball", { duration: 0.25, y: 60, scale: 0 });
--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,6 @@
 "use strict";
+import { gsap } from "gsap";
+
+let tl = gsap.timeline();
+
+tl.to(".ball", { duration: 2, x: 400, rotate: 200, duration: 2, ease: "slow" }).to(".ball", { duration: 2, y: -22, x: 413, rotate: 360 });
